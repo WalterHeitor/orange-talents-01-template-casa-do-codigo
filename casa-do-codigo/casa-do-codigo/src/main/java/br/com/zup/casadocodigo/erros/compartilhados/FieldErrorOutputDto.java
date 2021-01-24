ba051ad213e84dbd2567191cab894f26285596dc
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-24 01:58:19 -0300

    Update FieldErrorOutputDto.java

--- casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/erros/compartilhados/FieldErrorOutputDto.java
+++ casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/erros/compartilhados/FieldErrorOutputDto.java
@@ -1,4 +1,20 @@
 package br.com.zup.casadocodigo.erros.compartilhados;
 
 public class FieldErrorOutputDto {
+    private String field;
+    private String menssage;
+
+
+    public FieldErrorOutputDto(String field, String menssage) {
+        this.field = field;
+        this.menssage = menssage;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getMenssage() {
+        return menssage;
+    }
 }
