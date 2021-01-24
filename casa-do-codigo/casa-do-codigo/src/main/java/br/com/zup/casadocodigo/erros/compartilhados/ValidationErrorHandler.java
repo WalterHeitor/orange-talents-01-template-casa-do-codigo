82379a7a70f29245da87013bdca362c9a204f8cb
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-24 01:58:53 -0300

    Create ValidationErrorHandler.java
    confecção de erros compartilhados

---
+++ casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/erros/compartilhados/ValidationErrorHandler.java
@@ -0,0 +1,74 @@
+package br.com.zup.casadocodigo.erros.compartilhados;
+
+import br.com.zup.casadocodigo.autores.AutorRepository;
+import br.com.zup.casadocodigo.autores.ProibeEmailDuplicadoAutorValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+public class ValidationErrorHandler {
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private ProibeEmailDuplicadoAutorValidator proibeEmailDuplicadoAutorValidator;
+    @Autowired
+    private AutorRepository autorRepository;
+
+    public ValidationErrorHandler() {
+    }
+
+    public void init(WebDataBinder binder){
+        binder.addValidators(proibeEmailDuplicadoAutorValidator);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ValidationErrorsOutputDto handlerValidatioError(MethodArgumentNotValidException exception){
+        List<ObjectError> globalErrors = exception.getBindingResult().getGlobalErrors();
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+        return buildValidationErrors(globalErrors, fieldErrors);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ValidationErrorsOutputDto handlerValidatioError(BindException exception){
+        List<ObjectError> globalErrors = exception.getBindingResult().getGlobalErrors();
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+        return buildValidationErrors(globalErrors, fieldErrors);
+    }
+
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ExceptionHandler(IllegalStateException.class)
+//    public ValidationErrorsOutputDto illegalStateExceptionHandler(IllegalStateException exception){
+//
+//
+//        return buildValidationErrors(globalErrors, fieldErrors);
+//    }
+
+    private ValidationErrorsOutputDto buildValidationErrors(List<ObjectError>globalErrors,
+                                                            List<FieldError>fieldErrors){
+        ValidationErrorsOutputDto validationErrors = new ValidationErrorsOutputDto();
+        globalErrors.forEach(objectError -> validationErrors.addError(getErrorMessage(objectError)));
+        fieldErrors.forEach(error -> {
+            String errorMessage = getErrorMessage(error);
+            validationErrors.addFieldError(error.getField(), errorMessage);
+        });
+        return validationErrors;
+    }
+    private String getErrorMessage(ObjectError error){
+        return messageSource.getMessage(error, LocaleContextHolder.getLocale());
+    }
+}
