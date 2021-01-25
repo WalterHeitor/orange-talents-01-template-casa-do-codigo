9373b22e9de0667ce13760dc53b183888bbd72a5
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-25 00:30:16 -0300

    Update AutorRespostaDTO.java

--- casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/autores/AutorRespostaDTO.java
+++ casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/autores/AutorRespostaDTO.java
@@ -4,37 +4,37 @@ import java.time.LocalDateTime;
 
 public class AutorRespostaDTO {
 
-	 private String nome;
-	    private String email;
-	    private String descricao;
-	    private LocalDateTime dataCriacao;
+	private String nome;
+	private String email;
+	private String descricao;
+	private LocalDateTime dataCriacao;
 
-	    
+	public AutorRespostaDTO(String nome, String email, String descricao, LocalDateTime dataCriacao) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.descricao = descricao;
+		this.dataCriacao = dataCriacao;
+	}
 
-	    public AutorRespostaDTO(String nome, String email, String descricao, LocalDateTime dataCriacao) {
-			super();
-			this.nome = nome;
-			this.email = email;
-			this.descricao = descricao;
-			this.dataCriacao = dataCriacao;
-		}
+	public String getNome() {
+		return nome;
+	}
 
-		public String getNome() {
-	        return nome;
-	    }
+	public String getEmail() {
+		return email;
+	}
 
-	    public String getEmail() {
-	        return email;
-	    }
+	public String getDescricao() {
+		return descricao;
+	}
 
-	    public String getDescricao() {
-	        return descricao;
-	    }
-	    public LocalDateTime getDataCriacao() {
-			return dataCriacao;
-		}
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
 
-	    public static AutorRespostaDTO transformaEmDTO(Autor autor){
-	        return new AutorRespostaDTO(autor.getNome(), autor.getEmail(), autor.getDescricao(), autor.getDataCriacao());
-	    }
+	public static AutorRespostaDTO transformaEmDTO(Autor autor) {
+		return new AutorRespostaDTO(autor.getNome(), autor.getEmail(),
+				autor.getDescricao(), autor.getDataCriacao());
+	}
 }
