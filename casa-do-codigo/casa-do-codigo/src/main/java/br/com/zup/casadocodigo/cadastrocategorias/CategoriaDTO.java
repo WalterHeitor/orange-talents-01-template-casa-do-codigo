c374f24ff4d60037c0c0d0fa7880f1d3f89515c8
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-25 00:30:35 -0300

    Create CategoriaDTO.java

---
+++ casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/cadastrocategorias/CategoriaDTO.java
@@ -0,0 +1,31 @@
+package br.com.zup.casadocodigo.cadastrocategorias;
+
+import javax.validation.constraints.NotBlank;
+
+import br.com.zup.casadocodigo.validation.compartilhada.UniqueValue;
+
+public class CategoriaDTO {
+
+	@NotBlank
+	@UniqueValue(domainClass = Categoria.class, fieldName = "nome")
+	String nome;
+	
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+	public Categoria transformaParaObjeto() {
+		return new  Categoria(this.nome);
+	}
+
+}
