15f914df5ab91e99ff61d4ea9b67f1c41c671e6c
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-25 00:52:14 -0300

    Codigo limpo retirada de codigos

--- casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/erros/compartilhados/ValidationErrorHandler.java
+++ casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/erros/compartilhados/ValidationErrorHandler.java
@@ -1,7 +1,7 @@
 package br.com.zup.casadocodigo.erros.compartilhados;
 
-import br.com.zup.casadocodigo.autores.AutorRepository;
-import br.com.zup.casadocodigo.autores.ProibeEmailDuplicadoAutorValidator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,28 +10,18 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.List;
 @RestControllerAdvice
 public class ValidationErrorHandler {
     @Autowired
     private MessageSource messageSource;
 
-    @Autowired
-    private ProibeEmailDuplicadoAutorValidator proibeEmailDuplicadoAutorValidator;
-    @Autowired
-    private AutorRepository autorRepository;
 
     public ValidationErrorHandler() {
     }
 
-    public void init(WebDataBinder binder){
-        binder.addValidators(proibeEmailDuplicadoAutorValidator);
-    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
