6d32ee77d9a69ac3fc6aad8d13badc1dc85d79ce
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-26 17:54:45 -0300

    Update DetalheLivroController.java

--- casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/detalhelivro/DetalheLivroController.java
+++ casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/detalhelivro/DetalheLivroController.java
@@ -1,5 +1,12 @@
 package br.com.zup.casadocodigo.detalhelivro;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class DetalheLivroController {
 
+	@GetMapping
 }
