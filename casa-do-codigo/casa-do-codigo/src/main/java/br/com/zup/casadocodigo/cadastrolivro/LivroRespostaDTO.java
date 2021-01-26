6783a31d4ee4cd4c6606d0dbdce237674627793b
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2021-01-26 17:41:39 -0300

    Create LivroRespostaDTO.java

---
+++ casa-do-codigo/casa-do-codigo/src/main/java/br/com/zup/casadocodigo/cadastrolivro/LivroRespostaDTO.java
@@ -0,0 +1,101 @@
+package br.com.zup.casadocodigo.cadastrolivro;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class LivroRespostaDTO {
+
+
+	private String titulo;
+
+	private String resumo;
+
+	private String sumario;
+	
+	private BigDecimal preco;
+
+	private int numeroPaginas;
+
+	private String isbn;
+
+	private LocalDate dataPublicacao;
+
+	private Long idCategoria;
+
+	private Long idAutor;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public String getSumario() {
+		return sumario;
+	}
+	
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public int getNumeroPaginas() {
+		return numeroPaginas;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public LocalDate getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public Long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public Long getIdAutor() {
+		return idAutor;
+	}
+
+	
+
+	public LivroRespostaDTO(Livro livro) {
+		super();
+		this.titulo = livro.getTitulo();
+//		this.resumo = livro.getResumo();
+//		this.sumario = livro.getSumario();
+//		this.preco = livro.getPreco();
+//		this.numeroPaginas = livro.getNumeroPaginas();
+//		this.isbn = livro.getIsbn();
+//		this.dataPublicacao = livro.getDataPublicacao();
+//		this.idCategoria = livro.getCategoria().getId();
+//		this.idAutor = livro.getAutor().getId();
+	}
+
+	
+
+	public LivroRespostaDTO(String titulo, String resumo, String sumario, BigDecimal preco, int numeroPaginas,
+			String isbn, LocalDate dataPublicacao, Long idCategoria, Long idAutor) {
+		super();
+		this.titulo = titulo;
+		this.resumo = resumo;
+		this.sumario = sumario;
+		this.preco = preco;
+		this.numeroPaginas = numeroPaginas;
+		this.isbn = isbn;
+		this.dataPublicacao = dataPublicacao;
+		this.idCategoria = idCategoria;
+		this.idAutor = idAutor;
+	}
+
+	public static LivroRespostaDTO transformaEmDTO(Livro livro) {
+		return new LivroRespostaDTO(livro.getTitulo(), livro.getResumo(),
+				livro.getSumario(), livro.getPreco(), livro.getNumeroPaginas(),
+				livro.getIsbn(), livro.getDataPublicacao(), 
+				livro.getCategoria().getId(), livro.getAutor().getId());
+	}
+	
+}
